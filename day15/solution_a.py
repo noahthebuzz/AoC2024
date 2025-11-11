f9621de007ee13f1ba35932a644f1d88a7a4fb74
Noah Sarcevic <noah.sarcevic@gmail.com>
2025-11-11 15:58:22 +0100

    test: autogenerated file fetching and creation of python scripts

---
+++ day15/solution_a.py
@@ -0,0 +1,9 @@
+def main():
+    with open("input.txt") as f:
+        data = f.read().strip().splitlines()
+    # TODO: Implement solution for part A here
+    print("Part A:", data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day16/solution_b.py
@@ -0,0 +1,9 @@
+def main():
+    with open("input.txt") as f:
+        data = f.read().strip().splitlines()
+    # TODO: Implement solution for part B here
+    print("Part B:", data)
+
+
+if __name__ == "__main__":
+    main()
